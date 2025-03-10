6975fdc90e735bfaeeba178e38174d17f00a03b4
Tirtho Ray <tirthoray10@gmail.com>
2025-03-10 20:47:05 +0600

    create product

---
+++ src/app/modules/product/product.interface.ts
@@ -0,0 +1,11 @@
+export type TProduct ={
+    name:string;
+    price:number;
+    description:string;
+    quantity:number;
+    picture:string;
+    category: string;
+    manyPicture:string[];
+    isAvailable: boolean;
+
+}
