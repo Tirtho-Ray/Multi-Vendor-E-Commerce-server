8fff0785325135ffad9e26385e8b9a896866c5d7
Tirtho Ray <tirthoray10@gmail.com>
2025-05-07 21:45:48 +0600

    update get product

--- src/app/modules/product/product.services.ts
+++ src/app/modules/product/product.services.ts
@@ -4,11 +4,17 @@ import { Product } from "./product.model"
 const  createProductIntoDB  = async (payload: TProduct) =>{
     const product  = await Product.create(payload);
     return product;
+}
+
+const getAllProductIntoDB = async ( ) =>{
+    const result  = await Product.find();
+    return result;
 
 }
 
 
 
 export const ProductServices ={
-    createProductIntoDB
+    createProductIntoDB,
+    getAllProductIntoDB
 }
