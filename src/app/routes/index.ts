4e29d9bb9a9b207103d6aa11946f35047d48a63a
Tirtho Ray <tirthoray10@gmail.com>
2025-05-12 23:02:01 +0600

    add comment router

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,7 @@ import { AuthRoutes } from '../modules/Auth/auth.route';
 import { UserRoutes } from '../modules/User/user.route';
 import { ProductRouter } from './../modules/product/product.route';
 import { VendorRouter } from '../modules/ReqVendor/vendor.route';
+import { CommentRouter } from '../modules/Comment/comment.route';
 
 
 const router = express.Router();
@@ -23,7 +24,11 @@ const moduleRoutes = [
   {
     path: '/vendor',
     route: VendorRouter,
-  }
+  },
+  {
+    path: '/comment',
+    route: CommentRouter,
+  },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
