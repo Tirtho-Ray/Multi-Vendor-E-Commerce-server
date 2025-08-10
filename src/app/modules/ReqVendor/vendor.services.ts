ffbba04a64a936959f0dbde65d3d38a04af4f682
Tirtho Ray <tirthoray10@gmail.com>
2025-08-10 18:30:30 +0600

    add validation befor create vendor

--- src/app/modules/ReqVendor/vendor.services.ts
+++ src/app/modules/ReqVendor/vendor.services.ts
@@ -1,11 +1,33 @@
-import { TVendor } from "./vendor.interface"
-import { Vendor } from "./vendor.model"
+import { TVendor } from "./vendor.interface";
+import { Vendor } from "./vendor.model";
+import AppError from "../../errors/AppError";
+import httpStatus from "http-status";
+import { User } from "../User/user.model";
 
-const createVendorIntoDB = async ( payload:TVendor )=>{
-    const result  = await Vendor.create(payload);
-    return result;
+const createVendorIntoDB = async (payload: TVendor) => {
+  const user = await User.findById(payload.userId);
+
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, "User not found");
+  }
+
+  if (user.status !== "ACTIVE") {
+    throw new AppError(httpStatus.FORBIDDEN, "User is not active");
+  }
+
+ 
+  const existingVendor = await Vendor.findOne({ userId: payload.userId });
+
+
+  if (existingVendor && ["pending", "approved"].includes(existingVendor.status)) {
+    throw new AppError(httpStatus.BAD_REQUEST, "You have already applied to become a vendor");
+  }
+
+ 
+  const result = await Vendor.create(payload);
+  return result;
 };
 
-export const VendorServices ={
-    createVendorIntoDB
-}
+export const VendorServices = {
+  createVendorIntoDB,
+};
